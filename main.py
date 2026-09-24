8d7b8a12206eb6ef378cf2c8871c6abcb26ccda1
MArko <146667861+SanMartin1337@users.noreply.github.com>
2026-09-24 22:16:34 +0300

    тест html для проверки работы websocket. проверка передачи message в одной локальной сети

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ connected_clients: list[WebSocket] = []
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     connected_clients.append(websocket)
+
+    try:
+        while True:
+            #ждем сообщение от клиента
+            message = await websocket.receive_text()
+            for client in connected_clients: # переборка всех подключенных
+                # пропускаем самого отправителя(не отправляем эхом его же сообщение
+                if client != websocket:
+                    await client.send_text(message) # пересылаем сообщение всем
+    except WebSocketDisconnect:
+        # отключение клиента из списка
+        connected_clients.remove(websocket)
